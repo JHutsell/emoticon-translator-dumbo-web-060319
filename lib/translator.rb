7194f68f9ce0440794175a547ff735894ba2afc3
Justin Hutsell <hutselljustin@gmail.com>
2019-05-31 18:26:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,8 @@ end
 def get_japanese_emoticon(path, emoticon)
   # code goes here
   library = load_library(path)
-  
+  if library["get_emoticon"][emoticon] == nil 
+    puts 
 end
 
 def get_english_meaning
